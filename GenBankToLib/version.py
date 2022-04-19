54d567a4dce5cd51ac43f100643f4409f9f480eb
linsalrob <raedwards@gmail.com>
2022-04-19 14:59:40 +0930

    Correctly infering version

--- GenBankToLib/version.py
+++ GenBankToLib/version.py
@@ -2,6 +2,6 @@ import pkg_resources
 # this comes from setuptools
 
 try:
-    __version__ = pkg_resources.get_distribution('PhiSpy').version
+    __version__ = pkg_resources.get_distribution('genbank_to').version
 except Exception:
     __version__ = 'unknown'
