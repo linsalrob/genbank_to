7d6ac8fd87a465a9904c6d7624d8535e68ab3483
linsalrob <raedwards@gmail.com>
2026-01-08 17:32:18 +1030

    simplifying genbank to json and adding to the API

--- GenBankToLib/__init__.py
+++ GenBankToLib/__init__.py
@@ -4,10 +4,12 @@ Helper functions that convert genbank to different formats.
 
 from .genbank import genbank_to_faa, genbank_to_fna, genbank_to_orfs, genbank_to_ptt, genbank_to_functions
 from .genbank import genbank_to_gff, genbank_to_phage_finder, genbank_seqio, genbank_to_amrfinder
+from .genbank_to_json import genbank_to_json
 from .version import __version__
 
 __all__ = [
             'genbank_to_faa', 'genbank_to_fna', 'genbank_to_orfs', 'genbank_to_ptt', 'genbank_to_functions',
             'genbank_to_gff', 'genbank_to_phage_finder', 'genbank_seqio', 'genbank_to_amrfinder',
+            'genbank_to_json',
             '__version__'
            ]
